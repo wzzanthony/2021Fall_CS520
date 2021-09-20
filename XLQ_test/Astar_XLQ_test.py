926c97e20824b41ec5acd1dd66c0922097017c0d
LynchXLQ <linqixiao2014@163.com>
2021-09-20 14:51:31 -0400

    update,variable change

--- XLQ_test/Astar_XLQ_test.py
+++ XLQ_test/Astar_XLQ_test.py
@@ -1,34 +1,31 @@
 import math
 import queue
 import random
+import time
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Cell:
 
-
-
-class Cell():
-    """
-    create Node structure to generate tree
-    """
-
-    def __init__(self, Position: list, Father_node, Goal_cell):  # position = [x, y]
+    def __init__(self, position: tuple, goal_position: tuple, father_node=None):  # position = [x, y]
         self.position = [0, 0]
-        self.father_node = Father_node
+        self.father_node = father_node
 
         # Heuristic cost from n to Goal_cell: hn
-        # self.hn = math.sqrt((position[0] - goal_cell[0]) ** 2 + (position[1] - goal_cell[1]) ** 2)        # Use Euclidean Distance
-        self.hn = abs(Position[0] - Goal_cell[0]) + abs(Position[1] - Goal_cell[1])  # Use Manhattan Distance
-        # self.hn = max(abs(position[0] - goal_cell[0]), abs(position[1] - goal_cell[1]))                   # Use Chebyshev Distance
+
+        # Use Euclidean Distance
+        # self.hn = math.sqrt((position[0] - goal_cell[0]) ** 2 + (position[1] - goal_cell[1]) ** 2)
+
+        # Use Manhattan Distance
+        self.hn = abs(position[0] - goal_position[0]) + abs(position[1] - goal_position[1])
+
+        # Use Chebyshev Distance
+        # self.hn = max(abs(position[0] - goal_cell[0]), abs(position[1] - goal_cell[1]))
 
         # Real cost from Start_cell to n: g(n)
-        if Father_node is None:
-            gn = 0
+        if father_node is None:
+            self.gn = 0
         else:
-            gn = self.father_node.get_gn()
+            self.gn = self.father_node.get_gn() + 1
 
         # Total cost: fn
         self.fn = self.hn + self.gn
@@ -36,24 +33,25 @@ class Cell():
     def set_father_node(self, node_name):
         '''
         Set father node for the point
-        :param node_name:
-        :return:
         '''
         self.father_node = node_name
-        if self.father_node == None:
+        if node_name == None:
             self.gn = 0
         else:
-            self.gn += 1
+            self.gn = self.father_node.get_gn() + 1
+        # # For test purpose
+        print('Father node is: {}, current Gn = {}'.format(node_name,self.gn))
 
     def get_position(self):
         '''
         Get position information of the node
-        :return:
         '''
         return self.position
 
     def get_father_node(self):
-        # change current node to father node
+        '''
+        change current node to father node
+        '''
         return self.father_node
 
     def get_hn(self):
@@ -65,153 +63,186 @@ class Cell():
     def get_fn(self):
         return self.fn
 
-    def __gt__(self, other):
-        return self.fn > other.get_fn()
+    def __hash__(self):
+        return hash(tuple(self.position))
 
-    def __eq__(self, other):
-        return self.fn == other.get_fn()
+    def __eq__(self, other: "Cell"):
+        return tuple(self.position) == tuple(other.position)
 
+    def __str__(self):
+        return 'Cell({})'.format(self.position)
+
+    # For PriorityQueue
     def __lt__(self, other):
         return self.fn < other.get_fn()
 
 
-class Map():
-    obstacle_num = 5000  # obstacles number
+class Maze:
 
-    def __init__(self, Width, Height):
-        self.width = Width
-        self.height = Height
-        self.data = [[0 for i in range(Height)] for j in range(Width)]
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.data = [[0 for i in range(height)] for j in range(width)]
 
-    # Print out an empty maze
-    def maze_show(self):
+    def show_maze(self):
+        '''
+        Print out an empty maze
+        '''
         for i in range(self.width):
             for j in range(self.height):
                 print(self.data[i][j], end=' ')
             print(' ')
 
-    # Set obstacles with '#'
     def obstacle(self, x, y):
+        '''
+        Set obstacles with '#'
+        '''
         self.data[x][y] = "#"
 
-    # Print out a maze with obstacles
-    def maze_obstacles_show(self, Obstacle_X, Obstacle_Y):
-        for num in range(self.obstacle_num):
-            i = random.randint(0, (Obstacle_X - 1))
-            j = random.randint(0, (Obstacle_Y - 1))
+    def set_maze_obstacles(self, obstacle_num):
+        '''
+        Set the obstacles, there are obstacle_num obstacles
+        '''
+
+        # Check if the obstacles number is less than total cells number
+        if obstacle_num > self.width * self.height:
+            msg = 'Number of obstacles is {}, which is more than total number of cells {}'.format(obstacle_num, self.width * self.height)
+            raise AssertionError(msg)
+        # Set obstacles
+        for num in range(obstacle_num):
+            i = random.randint(0, (self.width - 1))
+            j = random.randint(0, (self.height - 1))
             self.obstacle(i, j)
-        self.maze_show()
+        self.show_maze()
 
-    # Set start cell with 'S'
     def draw_start(self, cell: Cell):
-        i = cell.get_position()[0]
-        j = cell.get_position()[1]
-        self.data[i]][j] = "S"
+        '''
+        Set start cell with 'S'
+        '''
+        i, j = cell.get_position()
+        self.data[i][j] = "S"
 
-    # Set goal cell with 'G'
     def draw_goal(self, cell: Cell):
-        self.data[cell.position[0]][cell.position[1]] = "G"
+        '''
+        Set goal cell with 'G'
+        '''
+        i, j = cell.get_position()
+        self.data[i][j] = "G"
+
+    def position_is_valid(self, i, j):
+        '''
+        Check if the position (i, j) is valid, an valid position means it's in the maze that
+        are not out of bound of the world
+        '''
+        return 0 <= i < self.height and 0 <= j < self.width
+
+    def is_obstacle(self, i, j):
+        '''
+        Check if the position (i, j) is an obstacle
+        '''
+        if self.data[i][j] == '#':
+            return True
+        return False
+
+    def generate_children(self, current_cell: Cell, goal_cell: Cell):
+        '''
+        Generate the children of n (neighbors believed or known to be unoccupied)
+        '''
+        dij = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        children_list = []
+        # get current location, and trying to explore the adjacent cells
+        i, j = current_cell.get_position()
+        for di, dj in dij:
+            ni, nj = i + di, j + dj
+            # the position is valid and it's not an obstacle
+            if self.position_is_valid(ni, nj) and not self.is_obstacle(ni, nj):
+                child = Cell(position=(ni, nj), goal_position=goal_cell.get_position(), father_node=current_cell)
+                children_list.append(child)
+        return children_list
 
 
 class Astar():
-    def __init__(self, Start_cell: Cell, Goal_cell: Cell, Map: Map):
-        self.start_cell = Start_cell  # Start point
-        self.goal_cell = Goal_cell  # Goal point
-        self.current_cell = Start_cell  # Current point
-        self.map = Map  # Map
-        self.open_list = queue.PriorityQueue()  # (fn, [x,y])
-        self.close_list = []
-        self.path_list = []
+    '''
+    Astar algorithm
+    '''
+    def __init__(self, start_cell: Cell, goal_cell: Cell, maze: Maze):
+        self.start_cell = start_cell  # Start point
+        self.goal_cell = goal_cell  # Goal point
+        self.maze = maze   # Whole maze
 
-    def get_cells_around(self):
-        cells_around_list = []
-        cell_up = Cell(Position=[self.current_cell.position[0] + 1, self.current_cell.position[1]], Father_node=,
-                       Goal_cell=goal_cell)
-        cell_down = Cell(Position=[self.current_cell.position[0] - 1, self.current_cell.position[1]], Father_node=,
-                         Goal_cell=goal_cell)
-        cell_right = Cell(Position=[self.current_cell.position[0], self.current_cell.position[1] + 1], Father_node=,
-                          Goal_cell=goal_cell)
-        cell_left = Cell(Position=[self.current_cell.position[0], self.current_cell.position[1] - 1], Father_node=,
-                         Goal_cell=goal_cell)
-        cells_around_list.extend([cell_up, cell_down, cell_right, cell_left])
-        return cells_around_list
-
-    def isin_openlist(self, Cell: Cell):
-        for cnt in range(self.open_list.qsize()):
-            each_cell = self.open_list.get()
-            if each_cell[1] == Cell.position:
-                return True
-        return False
-
-    def isin_closelist(self, Cell: Cell):
-        for item in self.close_list:
-            if item == Cell.position:
-                return True
-        return False
-
-    def is_obstacle(self, Cell: Cell):
-        if self.map.data[Cell.position[0]][Cell.position[1]] == '#':
-            return True
-        else:
-            return False
-
-    def get_available_cells(self, Cells_around_list: list):
-        available_cells_list = []
-        for each_cell in Cells_around_list:
-            # Test if cell in the maze
-            if 0 < each_cell.get_position()[0] <= self.map.width:
-                if 0 < each_cell.get_position()[1] <= self.map.height:
-                    # Test if cell can pass
-                    if each_cell not in self.close_list:
-                        available_cells_list.append(each_cell)
-        return available_cells_list
-
-    def search_next_cell(self, Cells_around_list: list, Available_cells_list: list):
+    def run_Astar(self):
         '''
-        Find a cell with least fn in the open_list and return it
-        :param Cells_around_list:
-        :param Available_cells_list:
-        :return:
+        Use a priority queue, always select the cell with smallest fn to explore
         '''
-        current_cell_fn = self.current_cell.get_fn()
-        # (fn, Cell) in PriorityQueue
-        self.open_list.put((current_cell_fn, self.current_cell))
-        # Check if cell is adjunct to the current cell
-        for cell in Cells_around_list:
-            # Check if cell is available
-            if cell in Available_cells_list:
-                # Put available cell in the open_list
-                self.open_list.put((cell.get_fn(), cell))
-        # Get a cell with least fn from the PriorityQueue
-        next_cell = self.open_list.get()
-        return next_cell
+        open_list = queue.PriorityQueue()
+        open_list.put((self.start_cell.fn, self.start_cell))    # (fn, cell)
+        closed_dict = dict()   # { current_cell : gn }
+        # When open_list is not empty
+        while not open_list.empty():
+            fn, current_cell = open_list.get()
+            # reach the goal cell
+            if current_cell.get_position() == self.goal_cell.get_position():
+                return self.get_path(current_cell)
+            # the current cell has been visited
+            if current_cell in closed_dict:
+                continue
+            closed_dict[current_cell] = current_cell.gn
+            # Generate the children of n (neighbors believed or known to be unoccupied)
+            for child_cell in self.maze.generate_children(current_cell, self.goal_cell):
+                # The successors of n are the children n0 that are newly discovered, or g(n0) > g(n) + 1.
 
-    def get_path(self, Next_cell: Cell):
+                # newly discovered, insert n0 into the open_list at priority f(n0) = g(n0) + h(n0),
+                if child_cell not in closed_dict:
+                    open_list.put((child_cell.fn, child_cell))
+                # g(n0) > g(n) + 1, insert n0 into open_list at priority f(n0) = g(n0) + h(n0),
+                elif child_cell.gn < closed_dict[child_cell]:
+                    closed_dict.pop(child_cell)
+                    open_list.put((child_cell.fn, child_cell))
+        # If no path found
+        return []
+
+    def get_path(self, current_cell: Cell):
         '''
-        Put
-        :param Next_cell:
-        :return:
+        Get the path
         '''
-        path_list = self.path_list.append(Next_cell.position)
-        self.map.data[Next_cell.position[0]][Next_cell.position[1]] = '*'
-        return path_list
+        path_list = []
+        while current_cell != self.start_cell:
+            path_list.append(current_cell)
+            current_cell = current_cell.father_node
+        path_list.append(self.start_cell)
+        # reverse the list
+        return path_list[::-1]
 
 
-if __name__ == '__main__':
-    start_cell = Cell(Position=[0, 0], Father_node=, Goal_cell=[100, 100])
-    goal_cell = Cell(Position=[100, 100], Father_node=, Goal_cell=[100, 100])
-    maze = Map(101, 101)
 
-    maze.maze_obstacles_show(101, 101)
+def main():
+    # size of maze
+    size = 101
+    # number of obstacles
+    num_obstacles = 2000
+    # start and goal
+    start_cell = Cell(position=(0, 0), goal_position=(size - 1, size - 1))
+    goal_cell = Cell(position=(size - 1, size - 1), goal_position=(size - 1, size - 1))
+    # Initialize maze
+    maze = Maze(size, size)
+    maze.set_maze_obstacles(num_obstacles)
     maze.draw_start(start_cell)
     maze.draw_goal(goal_cell)
-    print('-' * 150)
-    maze.maze_show()
+    astar = Astar(start_cell=start_cell,goal_cell=goal_cell,maze=maze)
+    # Calculate total time the A* research algorithm use
+    start_time = time.time()
+    path_list = astar.run_Astar()
+    print("Total search time:", time.time() - start_time)
 
-    astar_search = Astar(Start_cell=start_cell, Goal_cell=goal_cell, Map=maze)
-    cells_around_list = astar_search.get_cells_around()
-    available_cells_list = astar_search.get_available_cells(Cells_around_list=cells_around_list)
-    next_cell = astar_search.search_next_cell(Cells_around_list=cells_around_list,
-                                              Available_cells_list=available_cells_list)
-    path_list = astar_search.get_path(Next_cell=next_cell)
-    maze.maze_show()
+    if path_list is not []:
+        print('Path:', path_list)
+    else:
+        print('No path found!')
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
