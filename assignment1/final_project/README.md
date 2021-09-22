c7c40eb9e8934c6bba17f9068a74208b46591b2f
王锦潼 <wangjt@beagledata.com>
2021-09-22 01:07:05 -0400

    repeat forward A* code update

--- assignment1/final_project/README.md
+++ assignment1/final_project/README.md
@@ -36,3 +36,53 @@ if __name__ == '__main__':
 ```
 [[0, 0], [0, 1], [0, 2], [0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [5, 2], [6, 2], [6, 1], [7, 1], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], [8, 8], [8, 9], [9, 9]]
 ```
+
+
+## usage of the repeat forward A star algorithm interface
+
+### Description
+
+The repeat forward A star algorithm will repeat the route because it always starts at the cell where it encounters an obstacle in the last iteration. So the length of the path will much larger than the length of the shortest path.
+
+### parameter
+|name|info|type|options|
+|:----|:----|:----|:---|
+|maze|the information of the maze|list|/|
+|rows|count of the rows|int|/|
+|columns|count of the columns|int|/|
+|model|model to calculate distance|string|"E":"Euclidean Distance" (default)<br/>"M":"Manhattan Distance"<br/>"C":"Chebyshev Distance"|
+|start_cell|start cell of the maze|list|/|
+|end_cell|the goal of the maze|list|/|
+
+### using example
+```angular2html
+from Repeat_A_star import repeat_A_star
+if __name__ == '__main__':
+    maze=[[0,0,0,0,0,0,0,0,0,1],
+          [0,0,0,0,0,0,0,0,1,0],
+          [0,0,1,0,0,0,0,1,0,0],
+          [0,1,0,0,0,0,1,0,0,0],
+          [1,0,0,0,0,1,1,0,0,0],
+          [0,0,0,0,1,0,0,1,1,0],
+          [0,0,0,1,0,0,1,1,1,0],
+          [0,0,1,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0]]
+    
+    result=repeat_A_star(maze=maze,
+                         rows=10,
+                         columns=10,
+                         start_cell=[0,0],
+                         end_cell=[9,9],
+                         model='E')
+    print(len(result))
+    print(result)
+
+```
+
+### result example
+
+```
+[[0, 0], [1, 0], [1, 1], [2, 1], [1, 1], [1, 2], [1, 3], [2, 3], [3, 3], [3, 4], [4, 4], [3, 4], [3, 5], [2, 5], [2, 6], [1, 6], [1, 7], [0, 7], [0, 8], [0, 7], [1, 7], [1, 6], [2, 6], [2, 5], [3, 5], [3, 4], [4, 4], [4, 3], [5, 3], [5, 2], [6, 2], [6, 1], [7, 1], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], [8, 8], [8, 9], [9, 9]]
+
+```
---
+++ assignment1/final_project/Repeat_A_star.py
@@ -0,0 +1,80 @@
+from A_star import A_star_for_repeat
+
+
+def check_route(path: list, block_list: list, maze: list, rows: int, columns: int):
+    for index, each_cell in enumerate(path):
+        if maze[each_cell[0]][each_cell[1]] == 1:
+            if each_cell not in block_list:
+                block_list.append(each_cell)
+            path = path[:index]
+            break
+        else:
+            # check if the around cell of the current cell are obstacle
+            # TODO the structure of the code should be optimized, there are some duplicate code down here
+            # under one
+            if each_cell[0] + 1 < rows:
+                if maze[each_cell[0] + 1][each_cell[1]] == 1:
+                    if each_cell not in block_list:
+                        block_list.append([each_cell[0]+1, each_cell[1]])
+            # upper one
+            if each_cell[0] - 1 >= 0:
+                if maze[each_cell[0] - 1][each_cell[1]] == 1:
+                    if each_cell not in block_list:
+                        block_list.append([each_cell[0]-1, each_cell[1]])
+            # right one
+            if each_cell[1] + 1 < columns:
+                if maze[each_cell[0]][each_cell[1] + 1] == 1:
+                    if each_cell not in block_list:
+                        block_list.append([each_cell[0], each_cell[1]+1])
+            # left one
+            if each_cell[1] - 1 >= 0:
+                if maze[each_cell[0]][each_cell[1] - 1] == 1:
+                    if each_cell not in block_list:
+                        block_list.append([each_cell[0], each_cell[1]-1])
+    # delete the start cell
+    path = path[1:]
+    return path, block_list
+
+
+def repeat_A_star(maze: list, rows: int, columns: int, start_cell: list, end_cell: list, model='E'):
+    # record the cell that has been passed
+    path_list = [start_cell]
+    # record the obstacle that have been found
+    block_list = []
+    # record if the algorithm has found the path
+    status = False
+
+    while not status:
+        found_path = A_star_for_repeat(block_list=block_list,
+                                       rows=rows,
+                                       columns=columns,
+                                       start_cell=start_cell,
+                                       end_cell=end_cell,
+                                       model=model,
+                                       close_list=[])
+        # cannot found path, return no route
+        if len(found_path) == 0 and path_list[-1] != end_cell:
+            status = True
+            continue
+        # check if path is blocked, and find obstacles when iterating the path
+        worked_path_list, block_list = check_route(path=found_path,
+                                                   block_list=block_list,
+                                                   maze=maze,
+                                                   rows=rows,
+                                                   columns=columns)
+        # check if path has been found
+        if worked_path_list[-1] == end_cell:
+            # if the path has been found, return the found path
+            path_list.extend(worked_path_list)
+            status = True
+            # continue
+        else:
+            # if the path has not been found, change start cell to current cell and re-calculate the path
+            path_list.extend(worked_path_list)
+            start_cell = path_list[-1]
+
+    # test if the path is available
+    if path_list[-1] != end_cell:
+        return []
+    else:
+        return path_list
