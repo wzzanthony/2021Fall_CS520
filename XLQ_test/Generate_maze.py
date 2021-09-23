dc1fa3c39aa9e813e0cdef141043b8d87645964c
LynchXLQ <linqixiao2014@163.com>
2021-09-23 10:07:23 -0400

    update for Q5

--- XLQ_test/Generate_maze.py
+++ XLQ_test/Generate_maze.py
@@ -3,6 +3,7 @@ import random
 
 def generate_one_maze():
     data = [[0 for i in range(101)] for j in range(101)]
+
     positions = [(i, j) for i in range(0, 101) for j in range(0, 101)]
     for cell in positions:
         obstacle_probability = random.randint(0, 100)
@@ -17,21 +18,23 @@ def generate_one_maze():
             s = s.replace("'", '').replace(',', '').replace('\'','') + '\n'
             file.write(s)
         file.close()
-        print("保存文件成功")
+        print("*** Success ***")
 
 
 def generate_100_maze():
-    for p in range(100):
+    for p in range(0,100,10):
         data = [[0 for i in range(101)] for j in range(101)]
+
         positions = [(i, j) for i in range(0, 101) for j in range(0, 101)]
         for cell in positions:
             obstacle_probability = random.randint(0, 100)
             if obstacle_probability <= p:
                 data[cell[0]][cell[1]] = "#"
 
-        print(data)
-
-        with open("data.txt","a") as f:                                                   #设置文件对象
-            for i in data:                                                                 #对于双层列表中的数据
-                i = str(i).strip('[').strip(']').replace(',','').replace('\'','')+'\n'  #将其中每一个列表规范化成字符串
-                f.write(i)
+        with open("data.txt", "a") as file:
+            for i in range(len(data)):
+                s = str(data[i]).replace('[', '').replace(']', '')
+                s = s.replace("'", '').replace(',', '').replace('\'', '') + '\n'
+                file.write(s)
+            file.close()
+            print(f"*** Success {int(p/10)} ***")
